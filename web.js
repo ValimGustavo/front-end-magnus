cf9651a7a629019c77634c759fa444a550d9fe35
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-05-12 18:08:19 -0300

    alterado packjson

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 
-app.use(express.static(__dirname + "/dist/"))
+app.use(express.static(__dirname + "/dist/front-end-magnus"))
 
 app.listen(process.env.PORT || 3000, (req, res) => {
     console.log("rodando");
+    res.sendFile(path.join(__dirname+ '/dist/front-end-magnus/index.html'));
     
 })
