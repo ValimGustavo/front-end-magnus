10c1d86024e08013721f41f8aa89112292a4533e
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-05-13 09:54:34 -0300

    alteracao da url graphql

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -4,7 +4,7 @@ import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
 //TODO:TRANSFORMAR EM VARIAVEL DE AMBIENTE
-const uri = 'http://localhost:4444/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'https://magnus-legio-backend.herokuapp.com/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({uri}),
